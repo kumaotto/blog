38462d282dfb1b1c31b501be20d8c06dd2332403
kumaotto <kwkomaki.kom@gmail.com>
2023-03-26 02:37:08 +0900

    Add Head component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { InferGetStaticPropsType, NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { client } from "libs/client";
 import { Article, Category } from "types/blog";
