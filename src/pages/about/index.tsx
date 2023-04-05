fffaeec5f9b58202644cdf41bd9db01040e063b3
kumaotto <kwkomaki.kom@gmail.com>
2023-04-06 01:54:56 +0900

    update profile

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -14,10 +14,12 @@ const About: NextPage = () => {
             <h1 className="text-4xl text-gray-800">kumaotto</h1>
             <p>Kumi Komaki</p>
             <br />
-            <p className="mb-2">Job: Web application developer</p>
-            <p className="mb-2">Skills: Java, Javascript, TypeScript</p>
+            <p className="mb-2">Job: Web developer</p>
+            <p className="mb-2">Skills: Python, Java, JavaScript, TypeScript</p>
             <p>Qualifications</p>
             <ul className="list-disc pl-5 text-left w-2/3 sm:w-auto mx-auto sm:mx-0">
+              <li>2023 Mar. AWS Certified Solutions Architect - Associate</li>
+              <li>2023 Feb. AWS Certified Cloud Practitioner</li>
               <li>2022 Dec. 応用情報技術者試験</li>
               <li>2021 Aug. AWS Certified Developer - Associate</li>
             </ul>
