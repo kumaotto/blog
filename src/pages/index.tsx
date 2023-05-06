6dc44e3ba44f46dccdb88015d333b92e1ca53c5e
kumaotto <kwkomaki.kom@gmail.com>
2023-05-06 19:48:55 +0900

    Fix style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ const Home = ({
           <Suspense fallback={<Loading />}>
             <ul className="mt-4">
               {blogs.map((blog) => (
-                <li key={blog.id} className="rounded-md border-2 py-2.5">
+                <li key={blog.id} className="rounded-md border-2 py-2.5 mb-2">
                   <Link href={`/blog/${blog.id}`}>
                     <div className="text-left pl-3 sm:flex sm:items-center sm:justify-start sm:pl-5">
                       <p className="pr-7 text-neutral-500">{format(new Date(blog.updatedAt), 'yyyy年M月d日', {locale: ja})}</p>
