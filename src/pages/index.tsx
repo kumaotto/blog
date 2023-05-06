2148440101a8c5c93bd9522975b20c183f26c063
kumaotto <kwkomaki.kom@gmail.com>
2023-05-06 16:45:11 +0900

    Add Loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { InferGetStaticPropsType, NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { client } from "libs/client";
 import { Article, Category } from "types/blog";
 import Link from "next/link";
 import { Avatar } from "components/image/Avatar";
 import { ContentWrapper } from "components/ContentWrapper";
+import { Suspense } from "react";
+import Loading from "components/common/Loading/Loading";
 
 type Props = {
   blogs: Article[];
@@ -25,13 +25,15 @@ const Home = ({
           <h2 className="text-2xl font-bold text-gray-800 mt-10 text-left">
             Articles
           </h2>
-          <ul className="mt-4">
-            {blogs.map((blog) => (
-              <li key={blog.id} className="rounded-md border-2 py-2.5">
-                <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-              </li>
-            ))}
-          </ul>
+          <Suspense fallback={<Loading />}>
+            <ul className="mt-4">
+              {blogs.map((blog) => (
+                <li key={blog.id} className="rounded-md border-2 py-2.5">
+                  <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </Suspense>
         </div>
       </ContentWrapper>
     </>
