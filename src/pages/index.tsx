9a24aa29972c651cb148dda6e86c3fb736fc0a61
kumaotto <kwkomaki.kom@gmail.com>
2023-06-17 16:32:19 +0900

    Fix: ブログの日付を公開日に変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ const Home = ({
                 <li key={blog.id} className="rounded-md border-2 py-2.5 mb-2">
                   <Link href={`/blog/${blog.id}`}>
                     <div className="text-left pl-3 sm:flex sm:items-center sm:justify-start sm:pl-5">
-                      <p className="pr-7 text-neutral-500">{format(new Date(blog.createdAt), 'yyyy年M月d日', {locale: ja})}</p>
+                      <p className="pr-7 text-neutral-500">{format(new Date(blog.publishedAt), 'yyyy年M月d日', {locale: ja})}</p>
                       <p>{blog.title}</p>
                     </div>
                   </Link>
