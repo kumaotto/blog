a5e4438d9fcaff1b1399731c6d75dca804162ce8
kumaotto <kwkomaki.kom@gmail.com>
2024-05-01 02:29:37 +0900

    add qiita articles

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -2,6 +2,7 @@ import { createClient } from "microcms-js-sdk";
 import { normalizeZennArticle, ZennArticle } from "../types/ZennBlog";
 import { CommonArticle, PlatformIcon } from "../types/CommonBlog";
 import { MicroCMSArticle, MicroCMSArticles, normalizeCmsArticle } from "types/MicroCmsBlog";
+import { normalizeQiitaArticle, QiitaArticle } from "types/QiitaBlog";
 
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN || "",
@@ -19,6 +20,12 @@ export const getZennBlog: () => Promise<ZennArticle[]> = async () => {
   return data.articles;
 }
 
+export const getQiitaBlog: () => Promise<QiitaArticle[]> = async () => {
+  const res = await fetch("https://qiita.com/api/v2/items?page=1&per_page=10&query=Kumaou_00");
+  const data = await res.json();
+  return data;
+}
+
 export const getCmsBlogAndNormalized: () => Promise<CommonArticle[]> = async () => {
   const cmsArticles = await getMicroCMSBlog();
   const normalizedArticles = cmsArticles.map(article => ({
@@ -33,6 +40,15 @@ export const getZennBlogAndNormalized: () => Promise<CommonArticle[]> = async ()
   const normalizedArticles = zennArticles.map(article => ({
     ...normalizeZennArticle(article),
     icon: "zenn" as PlatformIcon,
+  }));
+  return normalizedArticles;
+}
+
+export const getQiitaBlogAndNormalized: () => Promise<CommonArticle[]> = async () => {
+  const qiitaArticles = await getQiitaBlog();
+  const normalizedArticles = qiitaArticles.map(article => ({
+    ...normalizeQiitaArticle(article),
+    icon: "qiita" as PlatformIcon,
   }));
   return normalizedArticles;
 }
