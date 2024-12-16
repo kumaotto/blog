9a918b019f29ed323f4dfdf5fc7d6f121f655b47
kumaotto <kwkomaki.kom@gmail.com>
2024-12-16 22:21:31 +0900

    update speakerdeck

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -19,7 +19,7 @@ const About: NextPage = () => {
               <p className="mb-2 w-fit mx-auto md:ml-0">Comments:</p>
               <p className="mb-2 w-fit mx-auto md:ml-0">  AWSで作るWebアプリケーション開発が一番得意です</p>
               <p className="mb-2 w-fit mx-auto md:ml-0">  アウトプット関連はフッターリンク参照</p>
-              <p className="mb-2 w-fit mx-auto md:ml-0">  Speaker Deck: <a className="text-blue-500" href="https://speakerdeck.com/kumaotto">https://speakerdeck.com/kumaotto</a></p>
+              <p className="mb-2 w-fit mx-auto md:ml-0">  Speaker Deck: <a className="text-blue-500" href="https://speakerdeck.com/komakichi">https://speakerdeck.com/komakichi</a></p>
               <br />
               <p className="w-fit mx-auto md:ml-0">Qualifications</p>
               <ul className="list-disc pl-5 text-left w-fit mx-auto sm:mx-0">
